8499d265a7dd8e3ae34cfedb6f94587c3d4d16bf
Sana <deiver.sanamori@gmail.com>
2025-09-01 16:53:51 -0300

    correções

--- AgendaCultural/src/services/LoginService.java
+++ AgendaCultural/src/services/LoginService.java
@@ -1,12 +1,9 @@
 package services;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import model.Usuario;
 
 public class LoginService {
 
--- AgendaCultural/src/services/UsuarioService.java
+++ AgendaCultural/src/services/UsuarioService.java
@@ -1,12 +1,9 @@
 package services;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Writer;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import model.Usuario;
